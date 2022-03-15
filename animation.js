e1156bb51d930959921f0dc7dbe8c770a1bee19b
AdaMaldonado <34352705+AdaMaldonado@users.noreply.github.com>
2022-03-14 23:47:47 -0400

    Add text

--- animation.js
+++ animation.js
@@ -8,20 +8,20 @@ var Scene = {
 
 Scene.start = function () {
 	// Get the canvas and it's context.
-    Scene.canvas = document.getElementById("myCanvas");
-    Scene.canvasContext = Scene.canvas.getContext("2d");
+    	Scene.canvas = document.getElementById("myCanvas");
+    	Scene.canvasContext = Scene.canvas.getContext("2d");
 
 	// Seup the eating scene to be displayed.
-    Scene.sprite = eating;
+    	Scene.sprite = eating;
 	
 	// Attach the image to be used for the sprite.
 	Scene.sprite.img = new Image();
-    Scene.sprite.img.src = Scene.sprite.src;
+    	Scene.sprite.img.src = Scene.sprite.src;
 
 	// Wait till the eating image is loaded before starting the animation.
 	Scene.sprite.img.onload = function() {		
 		Scene.sprite.offset=-Scene.sprite.frames[Scene.sprite.frame].frame.w;
-    	Scene.mainLoop();
+    		Scene.mainLoop();
 	}
 };
 
@@ -29,8 +29,9 @@ Scene.start = function () {
 document.addEventListener( 'DOMContentLoaded', Scene.start);
 
 Scene.clearCanvas = function () {
+	Scene.canvasContext.fillText("Eating Animation",10,30);
 	Scene.canvasContext.fillStyle = "white"
-    Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
+    	Scene.canvasContext.fillRect(0, 0, Scene.canvas.width, Scene.canvas.height);
 };
 
 Scene.mainLoop = function() {
